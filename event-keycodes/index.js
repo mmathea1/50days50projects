e144a1bbaaca4477bcf85d7762722cd69ff59c5a
mmathea1 <mingina.mathea075@gmail.com>
2023-05-16 22:48:50 +0300

    cleanup script

--- event-keycodes/index.js
+++ event-keycodes/index.js
@@ -1,15 +1,9 @@
-var intro = document.getElementById('intro');
-var eventBoxes = document.querySelectorAll('.event');
-var key = document.getElementById('key');
-var keyCode = document.getElementById('key-code');
-var code = document.getElementById('code');
-eventBoxes.forEach((box) => { box.style.display = "none"; });
 window.addEventListener("keydown", (e) => {
-    intro.style.display = "none";
-    eventBoxes.forEach((eventBox) => {
-        eventBox.style.display = "inline-flex";
+    document.getElementById('intro').style.display = "none";
+    document.querySelectorAll('.key').forEach((box) => {
+        box.style.display = "inline-flex";
     });
-    key.innerText = e.key == ' ' ? 'Space' : e.key;
-    keyCode.innerText = e.keyCode;
-    code.innerText = e.code;
+    document.getElementById('key').innerText = e.key == ' ' ? 'Space' : e.key;
+    document.getElementById('key-code').innerText = e.keyCode;
+    document.getElementById('code').innerText = e.code;
 });
